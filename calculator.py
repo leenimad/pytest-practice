2ccfbde51a37c8f3252cdbba8fe4e248d264b844
leenimad <95282984+leenimad@users.noreply.github.com>
2026-01-28 15:59:37 +0000

    fixes for practice purposes only

--- calculator.py
+++ calculator.py
@@ -1,3 +1,27 @@
-def add_numbers(a, b):
-    # This is supposed to add, but it subtracts!
-    return a - b
+# def future_age(age_input):
+#     # This will crash if age_input is a string!
+#     return int(age_input) + 10
+#  case sensitivity : 
+# def get_gender(letter):
+#     if letter.lower() == "m":
+#         return "Male"
+#     elif letter.lower() == "f":
+#         return "Female"
+#     else:
+#         return "Unknown"
+
+# def count_high_hr(hr_list):
+#     count = 0
+#     for hr in hr_list:
+#         if hr > 100:
+#             count = count + 1
+#     return count # BUG: This is inside the loop!
+
+def needs_urgent_care(temp, is_dehydrated):
+    # Requirement: BOTH must be true to be "Urgent"
+    if temp > 37.5 and is_dehydrated == True:
+        return "Urgent"
+    else:
+        return "Normal"
+    
+print (needs_urgent_care(38,False))    
--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,14 @@
-from calculator import add_numbers
-
-def test_addition():
-    assert add_numbers(5, 5) == 10
+# from calculator import get_gender
+# def test_gender():
+#     # If the user types a small 'm', it should still return 'Male'
+#     assert get_gender("m") == "Male"
+# from calculator import count_high_hr
+# def test_hr_count():
+#     patients = [110, 120, 80, 130]
+#     # There are 3 patients over 100.
+#     assert count_high_hr(patients) == 3
+from calculator import needs_urgent_care
+def test_urgent_care():
+    # This patient has a fever BUT is NOT dehydrated. 
+    # They should be "Normal" (according to our strict rule).
+    assert needs_urgent_care(38.0, False) == "Normal"
